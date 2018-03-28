1e7130fd199947e5344c8ef4e1a37b420741edce
sxs149331 <sxs149331@utdallas.edu>
2018-03-28 00:34:48 -0500

    Support for serving static files added

--- ServerStatusChecker.py
+++ ServerStatusChecker.py
@@ -1,10 +1,15 @@
 
 import requests
 from requests.exceptions import ConnectionError
+import subprocess as sp
 
 from time import sleep
 
 request_url = "http://eventdata.utdallas.edu/api/fields?api_key=CD75737EF4CAC292EE17B85AAE4B6&datasource=cline_phoenix_fbis"
+
+
+def restart_server():
+    sp.Popen("gnome-terminal -t web-server --working-directory=/home/ssalam/Desktop/spec-evet-data-server python app_v2.py", shell=True)
 
 retry_interval = 60 #seconds
 while True:
@@ -16,7 +21,7 @@ while True:
 
     except ConnectionError, e:
         print "Send E-mail Here"
-
+        restart_server()
         print e
         retry_interval = retry_interval * 2
 
