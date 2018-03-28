6264c374dbb7d13beceeadd86624b06c1ad4dc7f
sxs149331 <sxs149331@utdallas.edu>
2018-03-28 01:25:55 -0500

    Support for serving static files added

--- ServerStatusChecker.py
+++ ServerStatusChecker.py
@@ -9,7 +9,7 @@ request_url = "http://eventdata.utdallas.edu/api/fields?api_key=CD75737EF4CAC292
 
 
 def restart_server():
-    sp.Popen("gnome-terminal", shell=True) # env=dict(os.environ, DISPLAY=":0.0", XAUTHORITY="/home/ssalam/.Xauthority"))
+    sp.Popen(["gnome-terminal"], shell=True) # env=dict(os.environ, DISPLAY=":0.0", XAUTHORITY="/home/ssalam/.Xauthority"))
     #os.system("gnome-terminal -x python app_v2.py")
 retry_interval = 60 #seconds
 restart_server()
